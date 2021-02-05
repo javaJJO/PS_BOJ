e4c2744137e8c188129a761ede4baf40ffa8bb14
suVVin <suvvin97@gmail.com>
2021-02-05 10:37:41 +0900

    DAY7 21/02/05

--- PS_BOJ_WEEK_1/DAY6_10844.java
+++ PS_BOJ_WEEK_1/DAY6_10844.java
@@ -1,9 +1,19 @@
-
+import java.io.*;
 public class DAY6_10844 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N =Integer.parseInt(br.readLine());
+		int[] dp = new int [101];
+		dp[1]=9;
+		for(int i=2; i<=N; i++) {
+			dp[i]=(2*dp[i-1])-1;
+			dp[i] = dp[i]%1000000000;
+		}
+		bw.write(dp[N]);
+		br.close();
+		bw.flush();
+		bw.close();
 	}
-
 }
--- PS_BOJ_WEEK_1/DAY6_9095.java
+++ PS_BOJ_WEEK_1/DAY6_9095.java
@@ -12,7 +12,7 @@ public class DAY6_9095 {
 			dp[1] = 1;
 			dp[2] = 2;
 			dp[3] = 4;
-			for (int j = 4; j <= n; j++) {
+			for (int j = 4; j <= n; j++) { //j를 i로 잘못써서 한참 헤맸다ㅜ
 				dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
 			}
 			bw.write(dp[n]+"\n");
