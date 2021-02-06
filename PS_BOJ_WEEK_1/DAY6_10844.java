814b9035db4eac2787f6a5cd9a7f08e8955a8d38
suVVin <suvvin97@gmail.com>
2021-02-06 17:27:26 +0900

    DAY8 21/02/06

--- PS_BOJ_WEEK_1/DAY6_10844.java
+++ PS_BOJ_WEEK_1/DAY6_10844.java
@@ -5,15 +5,6 @@ public class DAY6_10844 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N =Integer.parseInt(br.readLine());
-		int[] dp = new int [101];
-		dp[1]=9;
-		for(int i=2; i<=N; i++) {
-			dp[i]=(2*dp[i-1])-1;
-			dp[i] = dp[i]%1000000000;
-		}
-		bw.write(dp[N]);
-		br.close();
-		bw.flush();
-		bw.close();
+		
 	}
 }
