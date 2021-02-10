6736af077fc62a04be0afda6df83bcecfd6e5901
suVVin <suvvin97@gmail.com>
2021-02-10 22:21:33 +0900

    ..

--- PS_BOJ_WEEK_2/DAY11_10844.java
+++ PS_BOJ_WEEK_2/DAY11_10844.java
@@ -1,9 +1,30 @@
+import java.util.*;
 
 public class DAY11_10844 {
 
 	public static void main(String[] args) {
-		// 이중배열을 사용하는 문제
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[][] dp = new long[101][11];
+		sc.close();
+		// dp[N][L] = dp[N - 1][L - 1] + dp[N - 1][L + 1]
+		// 길이 N, 마지막 숫자가 L일 경우
 
+		for (int i = 1; i <= 9; i++) {
+			dp[1][i] = 1;
+		}
+
+		for (int i = 2; i <= n; i++) {
+			dp[i][0] = dp[i - 1][1];
+			for (int j = 1; j <= 9; j++) {
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+			}
+		}
+
+		long sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += dp[n][i];
+		}
+		System.out.println(sum % 1000000000);
 	}
-
 }
