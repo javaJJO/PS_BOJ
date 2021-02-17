a28edb68d3eb2a3abaa39b44641039342d1dcf7e
suVVin <suvvin97@gmail.com>
2021-02-17 21:54:17 +0900

    DAY19 21/02/17

--- PS_BOJ_WEEK_2/DAY11_10844.java
+++ PS_BOJ_WEEK_2/DAY11_10844.java
@@ -1,30 +1,42 @@
-import java.util.*;
+import java.io.*;
 
 public class DAY11_10844 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long[][] dp = new long[101][11];
-		sc.close();
-		// dp[N][L] = dp[N - 1][L - 1] + dp[N - 1][L + 1]
-		// 길이 N, 마지막 숫자가 L일 경우
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i = 1; i <= 9; i++) {
-			dp[1][i] = 1;
+		int N = Integer.parseInt(br.readLine());
+		long[][] dp = new long[N + 1][10];
+		
+		dp[1][0]=0;// 0으로 시작하는 경우는 없음
+		
+		//i(N이)가 1일 때(한자리수 일때) 
+		for (int j = 1; j < 10; j++) { 
+			//여기서 j는 자릿수
+			dp[1][j] = 1;
 		}
 
-		for (int i = 2; i <= n; i++) {
-			dp[i][0] = dp[i - 1][1];
-			for (int j = 1; j <= 9; j++) {
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+		for (int i = 2; i <= N; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j == 0) { //자릿값이 0인 경우
+					dp[i][j] = dp[i - 1][j+1]%1000000000;
+				}
+				else if (j == 9) { //자릿값이 9인 경우
+					dp[i][j] += dp[i - 1][j-1]%1000000000;
+				}
+				else { //그 이외의 경우
+					dp[i][j] = (dp[i-1][j-1]+dp[i-1][j+1])%1000000000;
+				}
 			}
 		}
-
-		long sum = 0;
-		for (int i = 0; i < 10; i++) {
-			sum += dp[n][i];
+		long sum = 0; //경우의 수를 더해준다
+		for (int j = 0; j < 10; j++) { 
+			sum = (sum + dp[N][j]);
 		}
-		System.out.println(sum % 1000000000);
+		bw.write(String.valueOf(sum%1000000000));
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
--- PS_BOJ_WEEK_2/DAY19_11057.java
+++ PS_BOJ_WEEK_2/DAY19_11057.java
@@ -1,5 +1,5 @@
 
-public class DAY13_11057 {
+public class DAY19_11057 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
